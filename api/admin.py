f68267a636e1b6feb116cd7f4fb068edca3be233
Mani Khanuja <manikhanuja1981@hotmail.com>
2018-07-28 12:15:45 -0700

    Added the screenplay-api project within a docker container

---
+++ api/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Screenplay
+
+admin.site.register(Screenplay)
---
+++ api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Screenplay(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    year = models.CharField(max_length=30)
+    writer = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    language = models.CharField(max_length=50)
+    poster_url = models.URLField()
+    script_url = models.URLField()
+    imdb_rating = models.DecimalField(max_digits=4, decimal_places=2)
+    rotten_tomato_rating = models.DecimalField(max_digits=4, decimal_places=2)
+
